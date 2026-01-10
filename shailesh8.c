0742a63e889f43959f8c6a2f8ee1702da3b7672f
shailesh kumar pandit <shailesh.kumar.pandit@rungta.org>
2026-01-10 16:32:53 +0530

    Add files via upload

---
+++ shailesh8.c
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task 7 - Advanced CSS</title>
+    <style>
+        .box {
+            width: 300px;
+            height: 150px;
+            background-color: lightblue;
+            border: 2px solid navy;
+            margin: 20px auto;
+            padding: 20px;
+            box-sizing: border-box;
+            text-align: center;
+            line-height: 110px;
+            font-weight: bold;
+        }
+        body {
+            background-image: url('https://via.placeholder.com/600x400');
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <div class="box">CSS Box Model Example</div>
+</body>
+</html>
