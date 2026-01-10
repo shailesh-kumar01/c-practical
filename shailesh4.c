74b3729b402e9fa6732e02e54f69a9412e5585bb
shailesh kumar pandit <shailesh.kumar.pandit@rungta.org>
2026-01-10 16:30:47 +0530

    Add files via upload

---
+++ shailesh4.c
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task 3 - Table</title>
+</head>
+<body>
+    <table border="1" cellspacing="0" cellpadding="10" align="center">
+        <caption><strong>Student Marks Table</strong></caption>
+        <tr>
+            <th>Student</th>
+            <th>Math</th>
+            <th>Science</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>Saurabh</td>
+            <td>80</td>
+            <td>90</td>
+            <td>170</td>
+        </tr>
+        <tr>
+            <td>Rohit</td>
+            <td colspan="2" align="center">Absent</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>sonu</td>
+            <td>70</td>
+            <td>85</td>
+            <td>155</td>
+        </tr>
+    </table>
+</body>
+</html>
