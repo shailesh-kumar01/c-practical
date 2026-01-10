5e3e387d07a671df1ef9c5a10e7781bfef714aad
shailesh kumar pandit <shailesh.kumar.pandit@rungta.org>
2026-01-10 16:29:34 +0530

    Add files via upload

---
+++ shailesh2.c
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Task 1 - Basic HTML</title>
+</head>
+
+<body>
+    <h1>Name shailesh kumar pandit</h1>
+    <h2>ERP- 11327</h2>
+    <h3>Sub- Web Development</h3>
+
+    <p>My Name is shailesh kumar pandit.</p>
+
+</body>
+
+</html>
