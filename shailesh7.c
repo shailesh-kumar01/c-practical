e0f22dc315b2a9cb34b997f0210482e91ac3739e
shailesh kumar pandit <shailesh.kumar.pandit@rungta.org>
2026-01-10 16:32:24 +0530

    Add files via upload

---
+++ shailesh7.c
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task 6 - CSS</title>
+    <style>
+        /* Internal CSS */
+        h2 { color: blue; text-align: center; }
+        p { font-size: 16px; }
+    </style>
+    <!-- External CSS link example (file: style.css) -->
+    <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+</head>
+<body>
+    <h1 style="color: red;">First website</h1> <!-- Inline CSS -->
+    <h2>About Gole </h2>
+    <p>This is My gole.</p>
+</body>
+</html>
